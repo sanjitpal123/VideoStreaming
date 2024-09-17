38579422e87875770d2b750b350771f0b32ef985
aditya bhojane <adityabhojane66@gmail.com>
2024-09-17 10:56:26 +0530

    first commit project setup

---
+++ README.md
@@ -0,0 +1,8 @@
+setup contain
+
+- tailwind css
+- axios
+- react router dom
+- material Ui              ( npm install @mui/material @emotion/react @emotion/styled )
+- dotenv
+- Redux/ReduxToolkit ( not installed )
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import SimpleCollapse from "./Components/Demo/Demo"
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Welcome to Streaming app setup ( delete this when you start )</h1>
+     <SimpleCollapse/>
+    </>
+  )
+}
+
+export default App
