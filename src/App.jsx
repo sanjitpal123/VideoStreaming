c0b63c29c7dc807e5d1a6c7e4f5be98f355e8692
aditya bhojane <adityabhojane66@gmail.com>
2024-09-17 12:53:30 +0530

    first commit project setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
      <h1>Welcome to Streaming app setup ( delete this when you start )</h1>
      <SimpleCollapse/>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, quo.</p>
     </>
   )
 }
